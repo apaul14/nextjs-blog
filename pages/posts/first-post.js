99ac5076bdd19a5ed9f596a61ed9750d8082bf4d
Alex Paul <al.9thousand@gmail.com>
2021-04-26 11:54:01 -0400

    add blog section

---
+++ pages/posts/first-post.js
@@ -0,0 +1,23 @@
+ import Link from 'next/Link'
+ import Head from 'next/Head'
+ import Layout from '../../components/layout'
+ 
+ 
+ const FirstPost = () => {
+  return ( 
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to Home</a>
+          </Link>
+        </h2>
+    </Layout>
+  )
+}
+
+export default FirstPost
+
